5e47345d055350bbee2353a29d90b709f7db60cb
Karl J. Overå <k@rl.run>
2019-03-31 19:28:26 +0200

    More work on presentation

--- 0_workshop/presentation/src/presentation.js
+++ 0_workshop/presentation/src/presentation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { BlockQuote, Cite, Deck, Heading, ListItem, List, Quote, Image, Slide, Text } from 'spectacle'
+import { Appear, BlockQuote, Cite, Deck, Heading, ListItem, List, Quote, Image, Slide, Text, CodePane } from 'spectacle'
 
 import createTheme from 'spectacle/lib/themes/default'
 
@@ -19,6 +19,76 @@ const theme = createTheme(
   },
 )
 
+const code = {
+  graphql: `{
+  thing {
+    name
+    value
+    location
+  }
+}`,
+  graphql_res: `{
+  "thing": {
+    "name": "Very cool thing",
+    "value": 199,
+    "location": "Somewhere"
+  }
+}`,
+  nested: `{
+  "thing": {
+    "name": "Very cool thing",
+    "value": 199,
+    "owner": {
+      "name": "Per Son",
+      "country": {
+        "name": "Norway",
+        "region": "Nordic",
+        "climate": "Cold"
+      }
+    }
+  }
+}`,
+  multiple: `{
+  "thing": {
+    "name": "Very cool thing",
+    "value": 199
+  },
+  "weather": {
+    "current": "Sunny",
+    "tomorrow": "Rain"
+  }
+}`,
+  schema: `type Query {
+  thing(id: Int!): Thing
+  weather(location: String!): Weather
+}
+
+type Thing {
+  name: String!
+  value: Int
+  location: String
+  owner: [Person]!
+}
+
+type Person {
+  name: String!
+  country: Country!
+}
+
+type Country {
+  name: String!
+  region: String!
+  climate: Climate!
+  weather: Weather
+}
+
+type Weather {
+  current: String!
+  tomorrow: String
+}
+`,
+}
+
 export default class Presentation extends React.Component {
   render() {
     return (
@@ -36,6 +106,50 @@ export default class Presentation extends React.Component {
             GraphQL is a query language for APIs and a runtime for fulfilling those queries with your existing data.
           </Quote>
           <Cite textColor="primary">GraphQL.org</Cite>
+        </Slide>
+        <Slide bgColor="secondary">
+          <Heading size={4} lineHeight={1} textColor="primary">
+            Get exactly what you ask for
+          </Heading>
+          <Appear>
+            <div>
+              <Text textColor="primary">Request</Text>
+              <CodePane lang="graphql" source={code.graphql} theme="dark" />
+            </div>
+          </Appear>
+          <Appear>
+            <div>
+              <Text textColor="primary">Response</Text>
+              <CodePane lang="json" source={code.graphql_res} theme="dark" />
+            </div>
+          </Appear>
+        </Slide>
+        <Slide bgColor="secondary">
+          <Heading size={4} lineHeight={1} textColor="primary">
+            Ask for multiple resources in a single request
+          </Heading>
+          <Appear>
+            <div>
+              <Text textColor="primary">Deeply nested values </Text>
+              <CodePane lang="json" source={code.nested} theme="dark" />
+            </div>
+          </Appear>
+          <Appear>
+            <div>
+              <Text textColor="primary">Even several unrelated values</Text>
+              <CodePane lang="json" source={code.multiple} theme="dark" />
+            </div>
+          </Appear>
+        </Slide>
+        <Slide bgColor="secondary">
+          <Heading size={4} lineHeight={1} textColor="primary">
+            Everything represented by a strong type system
+          </Heading>
+          <Appear>
+            <div>
+              <CodePane lang="graphql" source={code.schema} theme="dark" />
+            </div>
+          </Appear>
         </Slide>
       </Deck>
     )
