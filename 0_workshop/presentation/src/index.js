c1a2fb72f749efe807d449bc9acae09aea4a388a
Karl J. Overå <k@rl.run>
2019-03-31 18:46:41 +0200

    Add now.json for deploying presentation

--- 0_workshop/presentation/src/index.js
+++ 0_workshop/presentation/src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import PropTypes from "prop-types";
-import ReactDOM from "react-dom";
-import { AppContainer } from "react-hot-loader";
-import Redbox from "redbox-react";
+import React from 'react'
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import Redbox from 'redbox-react'
 
-import Presentation from "./presentation";
+import Presentation from './presentation'
 
-const CustomErrorReporter = ({ error }) => <Redbox error={error} />;
+const CustomErrorReporter = ({ error }) => <Redbox error={error} />
 
 CustomErrorReporter.propTypes = {
-  error: PropTypes.instanceOf(Error).isRequired
-};
+  error: PropTypes.instanceOf(Error).isRequired,
+}
 
 ReactDOM.render(
   <AppContainer errorReporter={CustomErrorReporter}>
@@ -18,17 +18,17 @@ ReactDOM.render(
       <Presentation />
     </>
   </AppContainer>,
-  document.getElementById("root")
-);
+  document.getElementById('root'),
+)
 
 if (module.hot) {
-  module.hot.accept("./presentation", () => {
-    const NextPresentation = require("./presentation").default;
+  module.hot.accept('./presentation', () => {
+    const NextPresentation = require('./presentation').default
     ReactDOM.render(
       <AppContainer errorReporter={CustomErrorReporter}>
         <NextPresentation />
       </AppContainer>,
-      document.getElementById("root")
-    );
-  });
+      document.getElementById('root'),
+    )
+  })
 }
--- 0_workshop/presentation/src/presentation.js
+++ 0_workshop/presentation/src/presentation.js
@@ -1,39 +1,28 @@
-import React from "react";
+import React from 'react'
 
-import {
-  BlockQuote,
-  Cite,
-  Deck,
-  Heading,
-  ListItem,
-  List,
-  Quote,
-  Image,
-  Slide,
-  Text
-} from "spectacle";
+import { BlockQuote, Cite, Deck, Heading, ListItem, List, Quote, Image, Slide, Text } from 'spectacle'
 
-import createTheme from "spectacle/lib/themes/default";
+import createTheme from 'spectacle/lib/themes/default'
 
-require("normalize.css");
+require('normalize.css')
 
 const theme = createTheme(
   {
-    primary: "#EEEEEE",
-    secondary: "#1F2022",
-    tertiary: "#03A9FC",
-    quaternary: "#CECECE"
+    primary: '#EEEEEE',
+    secondary: '#1F2022',
+    tertiary: '#03A9FC',
+    quaternary: '#CECECE',
   },
   {
-    primary: "Montserrat",
-    secondary: "Helvetica"
-  }
-);
+    primary: 'Montserrat',
+    secondary: 'Helvetica',
+  },
+)
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["slide"]} transitionDuration={500} theme={theme}>
+      <Deck transition={['slide']} transitionDuration={500} theme={theme}>
         <Slide bgColor="secondary">
           <Heading size={1} fit caps lineHeight={1} textColor="primary">
             GraphQL workshop
@@ -44,12 +33,11 @@ export default class Presentation extends React.Component {
         </Slide>
         <Slide bgColor="tertiary">
           <Quote>
-            GraphQL is a query language for APIs and a runtime for fulfilling
-            those queries with your existing data.
+            GraphQL is a query language for APIs and a runtime for fulfilling those queries with your existing data.
           </Quote>
           <Cite textColor="primary">GraphQL.org</Cite>
         </Slide>
       </Deck>
-    );
+    )
   }
 }
