f6894f76a3101f23193fb8b38e5a0f4786e8656d
Aage Dahl <aage@gold-as.no>
2019-04-05 15:18:29 +0200

    kotlin init

---
+++ 2_examples/kotlin/src/main/kotlin/no/systek/graphqlworkshop/GraphqlworkshopApplication.kt
@@ -0,0 +1,11 @@
+package no.systek.graphqlworkshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlworkshopApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlworkshopApplication>(*args)
+}
