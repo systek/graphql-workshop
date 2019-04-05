8173c63f8dad8edc559063883e47aaf149f12935
Karl J. Overå <k@rl.run>
2019-04-05 20:29:35 +0200

    Add basic documentation for REST api

--- 0_workshop/fake_apis/marketprice/src/index.js
+++ 0_workshop/fake_apis/marketprice/src/index.js
@@ -1,19 +1,22 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0-83mfa.mongodb.net/test?retryWrites=true`;
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${
+  process.env.MONGODB_PASSWORD
+}@cluster0-83mfa.mongodb.net/test?retryWrites=true`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
-const connectToMongo = () => new Promise((resolve, reject) => {
-  client.connect(function (err) {
-    if (err != null) {
-      console.error(err);
-      return reject(err);
-    }
+const connectToMongo = () =>
+  new Promise((resolve, reject) => {
+    client.connect(function(err) {
+      if (err != null) {
+        console.error(err);
+        return reject(err);
+      }
 
-    console.info('Connected successfully to mongo database');
-    return resolve();
+      console.info("Connected successfully to mongo database");
+      return resolve();
+    });
   });
-});
 
 const getMarketPrice = (req, res) => {
   const { query } = parse(req.url, true);
@@ -21,39 +24,47 @@ const getMarketPrice = (req, res) => {
 
   if (ingredient === undefined) {
     res.statusCode = 400;
-    res.end('missing ingredient parameter');
+    res.end(JSON.stringify({ message: "missing ingredient parameter" }));
   }
 
-  connectToMongo().catch(reason => {
-    console.error(reason);
-    res.statusCode = 500;
-    res.end('Unable to connect to datasource');
-  }).then(() => {
-    const collection = client.db('gqlworkshop').collection('marketprice');
-    // perform actions on the collection object
-    collection.find({ 'ingredient': ingredient }).toArray()
-      .then(result => {
-        if (result === null) {
-          res.end({});
-        } else {
-          res.end(JSON.stringify(result));
-        }
-      })
-      .catch(reason => {
-        console.error('reason: ', reason);
-        res.statusCode = 500;
-        res.end('error reading from datasource');
-      });
-  });
+  connectToMongo()
+    .catch(reason => {
+      console.error(reason);
+      res.statusCode = 500;
+      res.end("Unable to connect to datasource");
+    })
+    .then(() => {
+      const collection = client.db("gqlworkshop").collection("marketprice");
+      // perform actions on the collection object
+      collection
+        .find({ ingredient: ingredient })
+        .toArray()
+        .then(result => {
+          if (result === null) {
+            res.end({});
+          } else {
+            res.end(JSON.stringify(result));
+          }
+        })
+        .catch(reason => {
+          console.error("reason: ", reason);
+          res.statusCode = 500;
+          res.end("error reading from datasource");
+        });
+    });
 };
 
-const { parse } = require('url');
+const { parse } = require("url");
 
-module.exports = (req, res) => { getMarketPrice(req, res) };
+module.exports = (req, res) => {
+  getMarketPrice(req, res);
+};
 
 if (!process.env.IS_NOW) {
-  const http = require('http');
-  const app = http.createServer((req, res) => { getMarketPrice(req, res) });
+  const http = require("http");
+  const app = http.createServer((req, res) => {
+    getMarketPrice(req, res);
+  });
 
   app.listen(3000, () => console.log(`Example app listening on port ${3000}!`));
 }
