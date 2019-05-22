f90b920dbf0db0aee9f9e7d476252b1f1358ba2e
Karl J. Overå <k@rl.run>
2019-05-22 18:57:35 +0200

    Add simple configuration to React starter

--- 1_starter/react/README.md
+++ 1_starter/react/README.md
@@ -2,6 +2,14 @@
 
 This starter uses:
 
-- create-react-app
-- TODO
+- create-react-app for simple React config
+- apollo-boost for simple apollo setup
 
+## Points of interest
+
+- [apollo/client.js](/1_starter/react/src/apollo/client.js)
+  - configuration of apollo client
+- [App.js](/1_starter/react/src/components/App.js)
+  - root component that has `ApolloProvider` with client
+- [Example.js](/1_starter/react/src/components/example/Example.js)
+  - example component that fetches a query on mount
---
+++ 1_starter/react/src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { ApolloProvider } from "react-apollo";
+
+import client from "../apollo/client";
+
+import Example from "./example/Example";
+import "./App.css";
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          React starter for GraphQL workshop
+        </header>
+        <p>Example component that fetches some data:</p>
+        <Example />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
