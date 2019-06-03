5912193c78a9c32ffdb8000fc8a96b09a7fbe3e1
Karl J. Overå <k@rl.run>
2019-06-03 20:53:32 +0200

    Configure react example to use URL per NODE_ENV

--- 3_examples/react/src/apollo/client.js
+++ 3_examples/react/src/apollo/client.js
@@ -1,13 +1,17 @@
 import ApolloClient, { InMemoryCache } from 'apollo-boost'
 
+const uri =
+  process.env.NODE_ENV !== 'production' ? '/' : 'https://node.gql.systek.dev'
+
 const cache = new InMemoryCache({
   dataIdFromObject: o => o.id,
 })
 
+console.log(uri)
+
 const client = new ApolloClient({
   // For dev with node example running on localhost:4000
-  // uri: '/',
-  uri: 'https://node.gql.systek.dev',
+  uri,
   cache,
 })
 
